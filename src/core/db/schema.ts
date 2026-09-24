ad5fc34d11d050ff3bf043f5e3d1836bedd5a341
DongminL <milkskfk12@gmail.com>
2026-09-24 15:51:19 +0900

    feat: FCM 토큰 테이블 추가

--- src/core/db/schema.ts
+++ src/core/db/schema.ts
@@ -1,3 +1,4 @@
 export * from "../../modules/account/domain/tables";
 export * from "../../modules/scheduling/domain/tables";
 export * from "../../modules/change-request/domain/tables";
+export * from "../../modules/notification/domain/tables";
